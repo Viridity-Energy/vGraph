a28d8f247a90cc0ffb0f70918c41ea4c1e121d7d
Brian Heilman <das.ist.junk@gmail.com>
2016-08-15 19:29:09 -0400

    allowing compare to load async

--- src/directives/vgraphCompare.js
+++ src/directives/vgraphCompare.js
@@ -20,32 +20,47 @@ require('angular').module( 'vgraph' ).directive( 'vgraphCompare',
 						'"></path></g>'
 					)[0];
 
-				$el[0].appendChild( element );
-				$compile( element )( scope );
+				function subscribe(){
+					if ( ref1 && ref2 && !unsubscribe ){
+						$el[0].appendChild( element );
+						$compile( element )( scope );
 
-				unsubscribe = graph.$on( 'focus-point', function( point ){
-					var p1 = point[ref1.view],
-						p2 = point[ref2.view],
-						view1 = ref1.$ops.$view,
-						view2 = ref2.$ops.$view,
-						v1 = ref1.$ops.getValue(p1),
-						v2 = ref2.$ops.getValue(p2);
+						unsubscribe = graph.$on( 'focus-point', function( point ){
+							var t,
+								p1 = point[ref1.view],
+								p2 = point[ref2.view],
+								view1 = ref1.$ops.$view,
+								view2 = ref2.$ops.$view,
+								v1 = ref1.$ops.getValue(p1),
+								v2 = ref2.$ops.getValue(p2);
 
-					point[ attrs.reference || 'compare' ] = {
-						value: Math.abs( v1 - v2 ),
-						y: ( view1.y.scale(v1) + view2.y.scale(v2) ) / 2,
-						x: ( p1.$x + p2.$x ) / 2
-					};
-				});
+							if ( (v1||v1===0) && (v2||v2===0) ){
+								t = {
+									value: Math.abs( v1 - v2 ),
+									y: ( view1.y.scale(v1) + view2.y.scale(v2) ) / 2,
+									x: ( p1.$x + p2.$x ) / 2
+								};
+							}else{
+								t = {
+									x: ( p1.$x + p2.$x ) / 2
+								};
+							}
+
+							point[ attrs.reference || 'compare' ] = t;
+						});
+					}
+				}
 
 				scope.$on('$destroy', unsubscribe );
 
 				scope.$watch('config1', function( cfg ){
 					ref1 = graph.getReference( cfg );
+					subscribe();
 				});
 
 				scope.$watch('config2', function( cfg ){
 					ref2 = graph.getReference( cfg );
+					subscribe();
 				});
 			}
 		};
