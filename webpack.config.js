a19078c2c86e6976de30739e72fac01027c29a6e
Shawn Rieger <riegersn@gmail.com>
2017-09-23 00:17:59 -0400

    ingore dist, some webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,20 @@
  * ...bundling vgraph for vpower
  */
 
+var fs = require('fs');
+var ver = JSON.parse(fs.readFileSync('./package.json')).version;
+var env = process.env.NODE_ENV
 var webpack = require('webpack');
+var min = '';
+
+plugins = [
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin() ];
+
+if (env === 'prod') {
+  min = `min.`
+  plugins.push(new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }));
+}
 
 module.exports = {
   entry: './vgraph.js',
@@ -16,15 +29,11 @@ module.exports = {
     }],
   },
   output: {
-    filename: './dist/vgraph.min.js',
+    filename: `./dist/vgraph-${ver}.${min}js`,
     library: 'vGraph',
     libraryTarget: "var"
   },
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-],
+  plugins: plugins,
   externals: {
     'd3': 'd3',
     'jquery': 'jQuery',
