464059b66aaaf0bc3ef7a66488303f30098a446a
Harish Selvaraj <harish.s@TVSi.Local>
2019-02-07 08:04:04 -0500

    fixed constant laoding message

--- src/directives/vgraphMessage.js
+++ src/directives/vgraphMessage.js
@@ -1,70 +1,70 @@
-var d3 = require('d3'),
-	angular = require('angular');
+var d3 = require("d3"),
+  angular = require("angular");
 
-angular.module( 'vgraph' ).directive( 'vgraphMessage',
-	[
-	function(){
-		return {
-			scope:{
-				customMessage:'=customMessage',
-			},
-			require : ['^vgraphChart'],
-			link : function( scope, el, attrs, requirements ){
-				var unsubscribe,
-					graph = requirements[0],
-					box = graph.box,
-					$el = d3.select( el[0] )
-						.attr( 'class', 'error-view' ),
-					$outline = $el.append( 'rect' )
-						.attr( 'class', 'outline' ),
-					$text = $el.append( 'text' );
+angular.module("vgraph").directive("vgraphMessage", [
+  function() {
+    return {
+      scope: {
+        customMessage: "=customMessage"
+      },
+      require: ["^vgraphChart"],
+      link: function(scope, el, attrs, requirements) {
+        var unsubscribe,
+          graph = requirements[0],
+          box = graph.box,
+          $el = d3.select(el[0]).attr("class", "error-view"),
+          $outline = $el.append("rect").attr("class", "outline"),
+          $text = $el.append("text");
 
-				$el.attr( 'visibility', 'hidden' );
+        $el.attr("visibility", "hidden");
 
-				box.$on('resize',function(){
-					if ( box.inner.height ){
-						$outline.attr( 'transform', 'translate('+box.inner.left+','+box.inner.top+')' )
-							.attr( 'width', box.inner.width )
-							.attr( 'height', box.inner.height );
+        box.$on("resize", function() {
+          if (box.inner.height) {
+            $outline
+              .attr(
+                "transform",
+                "translate(" + box.inner.left + "," + box.inner.top + ")"
+              )
+              .attr("width", box.inner.width)
+              .attr("height", box.inner.height);
 
             // different offset for zoom bar graph
             var center = box.center === 100 ? box.center - 10 : box.center;
 
-						try {
-              $text.attr('text-anchor', 'middle').attr('x', center).attr('y', box.middle + $text.node().getBBox().height / 2);
-						}catch( ex ){
-              $text.attr('text-anchor', 'middle').attr('x', center).attr('y', box.middle);
+            try {
+              $text
+                .attr("text-anchor", "middle")
+                .attr("x", center)
+                .attr("y", box.middle + $text.node().getBBox().height / 2);
+            } catch (ex) {
+              $text
+                .attr("text-anchor", "middle")
+                .attr("x", center)
+                .attr("y", box.middle);
             }
 
             if (box.inner.width <= 460) {
-              $text.attr('textLength', box.inner.width - 60);
+              $text.attr("textLength", box.inner.width - 60);
             }
-					}
-				});
+          }
+        });
 
-				scope.$watch('customMessage',function(msg){
-					if(msg){
-						$text.text( msg );
-						$el.attr( 'visibility', 'visible' );
-					}
-				});
+        scope.$watch("customMessage", function(msg) {
+          if (msg) {
+            $el.attr("visibility", "visible");
+            $text.text(msg);
+          } else {
+            $el.attr("visibility", "hidden");
+          }
+        });
 
-				function checkMessage(){
-					var msg = graph.message;
-
-					if (msg && (Object.keys(graph.views).length === Object.keys(graph.waitingOn).length)) {
-						$el.attr( 'visibility', 'visible' );
-						$text.text( msg );
-					}else{
-						$el.attr( 'visibility', 'hidden' );
-					}
-				}
-				unsubscribe = graph.$subscribe({
-					'error': checkMessage,
-					'rendered': checkMessage,
-					'configured': checkMessage
-				});
-			}
-		};
-	} ]
-);
+        function checkMessage() {
+          scope.customMessage = graph.message;
+        }
+        unsubscribe = graph.$subscribe({
+          error: checkMessage
+        });
+      }
+    };
+  }
+]);
